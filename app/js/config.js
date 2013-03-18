4f5686f6c96487b6badb3e8ebbe045a288bc8e9d
Mike Rayner <miker169@hotmail.co.uk>
2013-03-18 22:43:59 +0100

    added api key in for commnicating via google tasks api

---
+++ app/js/config.js
@@ -0,0 +1,7 @@
+define([], function() {
+  var config = {};
+  config.apiKey = 'iAIzaSyD3ltk1XGF-9hDm9-pG-bdzoGKQHJdDhXE';
+  config.scopes = 'https://www.googleapis.com/auth/tasks https://www.googleapis.com/auth/userinfo.profile';
+  config.clientId = '553970022573.apps.googleusercontent.com';
+  return config;
+});
